7299d7acb775d94475d0ba9df44a88e8dc5f76dd
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2024-08-10 10:48:47 +0200

    chore: Update UserRepository.scala to fix updateMeta and getById methods

--- src/main/g8/modules/server/src/main/scala/$base_folder$/$if(quill.truthy)$repositories$endif$/UserRepository.scala
+++ src/main/g8/modules/server/src/main/scala/$base_folder$/$if(quill.truthy)$repositories$endif$/UserRepository.scala
@@ -21,12 +21,12 @@ class UserRepositoryLive private (quill: Quill.Postgres[SnakeCase]) extends User
 
   inline given SchemaMeta[User] = schemaMeta[User]("users")
   inline given InsertMeta[User] = insertMeta[User](_.id)
-  inline given UpdateMeta[User] = updateMeta[User](_.id)
+  inline given UpdateMeta[User] = updateMeta[User](_.id, _.creationDate)
 
   override def create(user: User): Task[User] =
     run(query[User].insertValue(lift(user)).returning(r => r))
   override def getById(id: Long): Task[Option[User]] =
-    run(query[User].filter(_.id == Some(lift(id)))).map(_.headOption)
+    run(query[User].filter(_.id == lift(Option(id)))).map(_.headOption)
   override def getByEmail(email: String): Task[Option[User]] =
     run(query[User].filter(_.email == lift(email))).map(_.headOption)
 
@@ -34,12 +34,12 @@ class UserRepositoryLive private (quill: Quill.Postgres[SnakeCase]) extends User
     for {
       user <- getById(id).someOrFail(new RuntimeException(s"User \$id not found"))
       updated <- run(
-                   query[User].filter(_.id == Some(lift(id))).updateValue(lift(op(user))).returning(r => r)
+                   query[User].filter(_.id == lift(user.id)).updateValue(lift(op(user))).returning(r => r)
                  )
     } yield updated
 
   override def delete(id: Long): Task[User] =
-    run(query[User].filter(_.id == Some(lift(id))).delete.returning(r => r))
+    run(query[User].filter(_.id == lift(Option(id))).delete.returning(r => r))
 }
 
 object UserRepositoryLive {
